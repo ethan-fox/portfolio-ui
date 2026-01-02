c59b85bd7dcb9bac84b212759eb40485c76a06e9
Ethan Fox <ethan78fox@gmail.com>
2026-01-02 15:45:58 -0500

    Adjust styling

--- src/components/domain/HomeBanner.tsx
+++ src/components/domain/HomeBanner.tsx
@@ -5,7 +5,7 @@ const HomeBanner = () => {
     <div className="bg-primary py-[clamp(3rem,8vh,6rem)]">
       <div className="space-y-8">
         <div>
-          <h1 className="text-lg sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-mono font-semibold text-center max-w-[clamp(20rem,90vw,48rem)] mx-auto whitespace-nowrap">
+          <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-mono font-semibold text-center max-w-[clamp(20rem,90vw,48rem)] mx-auto whitespace-nowrap">
             ethan-builds <RotatingDomainText />
           </h1>
         </div>
